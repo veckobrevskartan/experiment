d946a5c1d4b4d804ea77d974a6b44d0741c2f2c7
veckobrevskartan <veckobrevskartan@proton.me>
2026-01-08 21:59:33 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+// data.js
+const events = [
+  {country:'SE',cat:'HYBRID',title:'Misstänkta konton söker drönarpiloter...',place:'Sverige (online)',date:'2025-10-05',lat:59.334,lng:18.067,summary:'Nya misstänkta Facebook-konton...',url:'https://www.sverigesradio.se/artikel/…',source:'Sveriges Radio (2025)'},
+  {country:'SE',cat:'NUCLEAR',title:'Ryskt uranfartyg med motorhaveri...',place:'31 km SO om Hasslö, Blekinge (SE)',date:'2025-09-29',lat:56.118,lng:15.585,summary:'Panamaflaggat fartyg Mikhail Dudin ligger för ankar...',url:'https://www.sydostran.se/…',source:'Sydöstran (2025)'},
+  // ... fler objekt som i 'index (34).html'
+];
